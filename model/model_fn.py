ae746bfc4a1f1a67a047e9c04108ac125db8a39b
Taylor Shin <taylorshin921@gmail.com>
2019-01-25 10:45:11 -0800

    fixed bugs in model and now model is trainable

--- model/model_fn.py
+++ model/model_fn.py
@@ -4,71 +4,45 @@ from tensorflow.keras import layers
 from constants import *
 
 def build_model():
-    # model = keras.Sequential([
-    #     layers.Conv2D(3, 32, 7),
-    #     layers.LeakyReLU(),
-    #     layers.MaxPooling2D(pool_size=(2, 2)),
-    #     layers.Conv2D(32, 32, 5),
-    #     layers.LeakyReLU(),
-    #     layers.MaxPooling2D(pool_size=(2, 2)),
-    #     layers.Conv2D(32, 32, 5),
-    #     layers.LeakyReLU(),
-    #     layers.MaxPooling2D(pool_size=(2, 2)),
-    #     layers.Conv2D(32, 32, 5),
-    #     layers.LeakyReLU(),
-    #     layers.MaxPooling2D(pool_size=(2, 2)),
-    #     layers.Conv2D(32, 32, 5),
-    #     layers.LeakyReLU(),
-    #     layers.MaxPooling2D(pool_size=(2, 2)),
-    #     layers.Conv2D(32, 32, 3),
-    #     layers.LeakyReLU(),
-    #     layers.MaxPooling2D(pool_size=(2, 2))
-    # ])
-    # model.summary()
-
     # Input layer
     inputs = keras.Input(shape=(IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
 
     # Layer 1
-    x = layers.Conv2D(3, 32, 7)(inputs)
-    x = layers.LeakyReLU()(x)
+    x = layers.Conv2D(32, 7)(inputs)
+    x = layers.LeakyReLU(alpha=0.1)(x)
     x = layers.MaxPooling2D(pool_size=(2, 2))(x)
 
     # Layer 2
-    x = layers.Conv2D(32, 32, 5)(inputs)
-    x = layers.LeakyReLU()(x)
+    x = layers.Conv2D(32, 5)(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
     x = layers.MaxPooling2D(pool_size=(2, 2))(x)
 
     # Layer 3
-    x = layers.Conv2D(3, 32, 5)(inputs)
-    x = layers.LeakyReLU()(x)
+    x = layers.Conv2D(32, 5)(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
     x = layers.MaxPooling2D(pool_size=(2, 2))(x)
 
     # Layer 4
-    x = layers.Conv2D(3, 32, 5)(inputs)
-    x = layers.LeakyReLU()(x)
+    x = layers.Conv2D(32, 5)(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
     x = layers.MaxPooling2D(pool_size=(2, 2))(x)
 
     # Layer 5
-    x = layers.Conv2D(3, 32, 5)(inputs)
-    x = layers.LeakyReLU()(x)
+    x = layers.Conv2D(32, 5)(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
     x = layers.MaxPooling2D(pool_size=(2, 2))(x)
 
     # Layer 6
-    x = layers.Conv2D(3, 32, 3)(inputs)
-    x = layers.LeakyReLU()(x)
+    x = layers.Conv2D(32, 3)(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
     x = layers.MaxPooling2D(pool_size=(2, 2))(x)
 
-    # print('before: ', x)
-    # x = layers.Flatten()(x)
-    # print('after: ', x)
-
     # Classification layers
-    x = layers.Reshape((-1, 32 * 7 * 12))(x)
+    x = layers.Flatten()(x)
     x = layers.Dense(4096, activation='softmax')(x)
+    x = layers.LeakyReLU(alpha=0.1)(x)
     x = layers.Dense(S1 * S2 * T, activation='softmax')(x)
     outputs = layers.Reshape((S1, S2, T))(x)
-    print('outputs: ', outputs)
 
     model = keras.Model(inputs=inputs, outputs=outputs)
     optimizer = tf.train.AdamOptimizer()
